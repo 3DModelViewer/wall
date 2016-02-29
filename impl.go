d471f68562be949ed473e2b94ec491e4229d9759
Daniel Robinson <daniel.robinson@autodesk.com>
2016-02-29 09:43:05 +0000

    init

---
+++ impl.go
@@ -0,0 +1,111 @@
+package wall
+
+import (
+	"github.com/yohcop/openid-go"
+	"net/http"
+	"net/url"
+	"github.com/modelhub/session"
+	"github.com/modelhub/core"
+	"strings"
+)
+
+const (
+	apiPathPrefix = "/api/"
+	rootPath = "/"
+	loginPath = "/openid/login"
+	callbackPath = "/openid/callback"
+)
+
+func NewWall(coreApi core.CoreApi, restApi *http.ServeMux, getSession session.SessionGetter, openidProviderEndpoint string, currentHost string, staticFileDir string) *http.ServeMux {
+	nonceStore := openid.NewSimpleNonceStore()
+	discoveryCache := openid.NewSimpleDiscoveryCache()
+	fileServer := http.FileServer(http.Dir(staticFileDir))
+
+	isLoggedIn := func(w http.ResponseWriter, r *http.Request) bool {
+		if s, err := getSession(w, r); err != nil || s == nil {
+			return false
+		} else if userId, err := s.User(); err != nil || userId == "" {
+			return false
+		}
+		return true
+	}
+
+	openidLoginHandler := func(w http.ResponseWriter, r *http.Request) {
+		if s, _ := getSession(w, r); s != nil {
+			s.Expire()
+		}
+		if redirectUrl, err := openid.RedirectURL(openidProviderEndpoint, currentHost+callbackPath, ""); err == nil {
+			values := make(url.Values)
+
+			values.Add("openid.ns.pape", "http://specs.openid.net/extensions/pape/1.0")
+			values.Add("openid.pape.max_auth_age", "0")
+
+			values.Add("openid.ns.sreg", "http://openid.net/extensions/sreg/1.1")
+			values.Add("openid.sreg.required", "email,fullname,nickname")
+
+			values.Add("openid.ns.ax", "http://openid.net/srv/ax/1.0")
+			values.Add("openid.ax.mode", "fetch_request")
+
+			values.Add("openid.ax.required", "img176,userid")
+			values.Add("openid.ax.type.img176", "http://axschema.org/autodesk/media/image/176")
+			values.Add("openid.ax.type.userid", "http://axschema.org/autodesk/userid")
+
+			http.Redirect(w, r, redirectUrl+"&"+values.Encode(), 303)
+		} else {
+			http.Error(w, "error in openidLoginHandler", 500)
+		}
+	}
+
+	openidCallbackHandler := func(w http.ResponseWriter, r *http.Request) {
+		if isLoggedIn(w, r) {
+			http.Redirect(w, r, rootPath, 303)
+		} else {
+			fullUrl := currentHost + r.URL.String()
+			id, err := openid.Verify(fullUrl, discoveryCache, nonceStore)
+
+			if err == nil {
+				parsedURL, _ := url.Parse(fullUrl)
+				values, _ := url.ParseQuery(parsedURL.RawQuery)
+				autodeskId := values.Get("openid.alias3.value.alias2")
+				if autodeskId == "" {
+					http.Error(w, "error in openidCallbackHandler, missing user id", 500)
+					return
+				}
+				openId		:= id
+				username	:= values.Get("openid.sreg.nickname")
+				avatar		:= values.Get("openid.alias3.value.alias1")
+				fullName	:= values.Get("openid.sreg.fullname")
+				email		:= values.Get("openid.sreg.email")
+
+				if user, err := coreApi.User().Login(autodeskId, openId, username, avatar, fullName, email); err != nil {
+					http.Error(w, "error in openidCallbackHandler", 500)
+				} else {
+					s, _ := getSession(w, r)
+					s.SetUser(user.Id)
+					http.Redirect(w, r, rootPath, 303)
+				}
+			} else {
+				http.Error(w, "error in openidCallbackHandler", 500)
+			}
+		}
+	}
+
+	outerHandler := func(w http.ResponseWriter, r *http.Request) {
+		if isLoggedIn(w, r) {
+			if strings.HasPrefix(r.URL.Path, apiPathPrefix) {
+				restApi.ServeHTTP(w, r)
+			} else {
+				fileServer.ServeHTTP(w, r)
+			}
+		} else {
+			http.Redirect(w, r, loginPath, 303)
+		}
+	}
+
+	outerRouter := http.NewServeMux()()
+	outerRouter.HandleFunc(loginPath, openidLoginHandler)
+	outerRouter.HandleFunc(callbackPath, openidCallbackHandler)
+	outerRouter.HandleFunc(rootPath, outerHandler)
+
+	return outerRouter
+}
